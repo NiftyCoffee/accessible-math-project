ff268d1f9252d6b9c7b40de154246151f0dfd493
thienautran <thien.autran75@gmail.com>
2023-07-12 14:33:32 +1000

    feat: added basic functionality to input LaTeX and render MathJax on input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import './App.css'
+import React, {useEffect, useState} from 'react';
+import './App.css';
 
 function App() {
+  const data = "When \\(a \\ne 0\\), there exists two solutions for\\(ax^2 + bx + c = 0\\) as \\[x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.\\]";
+  
+  const [latex, setLatex] = useState("");
+  
+  // rerender dom every time new hydrated math input state is given
+  useEffect(()=>{
+    if(typeof window?.MathJax !== "undefined"){
+      window.MathJax.typesetClear()
+      window.MathJax.typeset()
+    }
+  }, [latex]);
 
   return (
     <>
+      <h1>Hello math</h1>
+      <div>
+      <p>Some mathjax rendered content</p>
+
+      <p>
+        {data}
+      </p>
+      </div>
+
+      <div>
+        Enter some latex
+
+        <input onChange={(e)=> {
+        setLatex(e.target.value) }}/>
       
+      <h2>The rendered math from input</h2>
+      <p>{latex}</p>
+      </div>
     </>
   )
 }
